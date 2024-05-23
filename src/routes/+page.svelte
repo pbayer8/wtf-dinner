e82f55735a1b2e964f02958267d86874dc08f417
Phil <19184024+pbayer8@users.noreply.github.com>
2024-05-22 21:33:07 -0600

    images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
+  import { browser } from "$app/environment";
   import { preparations, proteins, seasonings, vegetables } from "$lib/data";
   import { inject } from "@vercel/analytics";
   import "./styles.css";
-
   inject();
-
   const pick = (array: string[]) =>
     array[Math.floor(Math.random() * array.length)];
 
@@ -12,11 +11,26 @@
   let vegetable = pick(vegetables);
   let seasoning = pick(seasonings);
   let preparation = pick(preparations);
+  $: fullSentence = `${protein} and ${vegetable} ${preparation} with ${seasoning}`;
+  let imgSrc: string | undefined;
+  const setImgSrc = async (prompt: string) => {
+    imgSrc = undefined;
+    if (prompt.length === 0) return;
+    if (!browser) return;
+    const response = await fetch("/image", {
+      method: "POST",
+      body: JSON.stringify({ prompt }),
+    });
+    const data = await response.json();
+    imgSrc = data.src;
+  };
+  $: setImgSrc(fullSentence);
 </script>
 
 <main class="flex h-screen w-screen items-center justify-center bg-neutral-200">
   <div
-    class="flex w-3/4 flex-col items-start justify-center gap-4 rounded-lg bg-white p-4 shadow-2xl"
+    class="flex w-3/4 flex-col items-start justify-center gap-4 rounded-lg
+    bg-white p-4 shadow-2xl"
   >
     <p class="text-xl">
       WTF is for dinner? <button
@@ -50,5 +64,12 @@
         on:click={() => (seasoning = pick(seasonings))}>{seasoning}</button
       >.
     </p>
+    {#if imgSrc}
+      <img
+        src={imgSrc}
+        alt="WTF is for dinner?"
+        class="max-h-[75vh] w-full max-w-[75vw] object-contain object-left"
+      />
+    {/if}
   </div>
 </main>
---
+++ src/routes/image/+server.ts
@@ -0,0 +1,18 @@
+import { env } from "$env/dynamic/private";
+import { json } from "@sveltejs/kit";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: env.OPENAI_API_KEY!,
+});
+export const POST = async ({ request }) => {
+  const { prompt } = await request.json();
+  const response = await openai.images.generate({
+    model: "dall-e-3",
+    prompt: `A photo of a plate of ${prompt}.`,
+    n: 1,
+    size: "1024x1024",
+  });
+  const src = response.data[0].url;
+  return json({ src });
+};
